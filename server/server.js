a2a5760312328952bd6dd32080b152fb06338f8a
Remy Ponso <remy@central>
2017-05-24 11:16:05 -0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express=require('express');
-var bodyParser=require('body-parser');
+const _ =require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
 const {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose');
@@ -66,6 +67,30 @@ app.delete('/todos/:id',(req,resp)=>{
     },()=>resp.status(400).send());
 });
 
+
+app.patch('/todos/:id',(req,resp)=>{
+    var id=req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return resp.status(404).send();
+    }
+    var body=_.pick(req.body,['completed','text']);
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt=new Date().getTime();
+    } else {
+        body.completed=false;
+        body.completedAt=null;
+    }
+    Todo.findByIdAndUpdate(id,{
+        $set:body
+    },{new:true}).then((todo)=>{
+        if(!todo) {
+            return resp.status(404).send();
+        }
+        resp.send({todo});
+    }).catch(()=>resp.status(400).send());
+    // console.log('body',body);
+    // resp.send('ok');
+});
 
 
 app.listen(port,()=>{
